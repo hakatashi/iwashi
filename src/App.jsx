ce9dca047c39381af9853a8e3a414777baaadf36
Koki Takahashi <hakatasiloving@gmail.com>
2017-12-15 14:25:15 +0900

    Fix score of base part

--- src/App.jsx
+++ src/App.jsx
@@ -99,8 +99,8 @@ module.exports = class App extends React.Component {
 				score: 'base',
 				videoStart: 4.8,
 				videoDuration: 0.5,
-				volume: 0.5,
-				sourceNote: 34,
+				volume: 0.3,
+				sourceNote: 22,
 				prank: true,
 			},
 			{
--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
 module.exports.getSoundUrls = (id) => (
 	process.env.NODE_ENV === 'production' ? [
-		`https://media.githubusercontent.com/media/hakatashi/iwashi/master/sound/${id}.ogg`,
-		`https://media.githubusercontent.com/media/hakatashi/iwashi/master/sound/${id}.wav`,
+		`https://media.githubusercontent.com/media/hakatashi/iwashi/gh-pages/sound/${id}.ogg`,
+		`https://media.githubusercontent.com/media/hakatashi/iwashi/gh-pages/sound/${id}.wav`,
 	] : [
 		`sound/${id}.ogg`,
 		`sound/${id}.wav`,
