1a286d7666f610acab22e37b5e9e51a4f58de7b2
Koki Takahashi <hakatasiloving@gmail.com>
2017-12-19 05:18:24 +0900

    Add more scores

--- src/Sound.jsx
+++ src/Sound.jsx
@@ -61,21 +61,25 @@ module.exports = class Sound extends React.Component {
 	}
 
 	handleBeat = (beat) => {
-		if (Math.abs((beat + TICK) % (TICK * 1536) - TICK) < TICK / 2) {
+		if (Math.abs((beat + TICK) % (TICK * 1792) - TICK) < TICK / 2) {
 			this.setState({isShown: false});
 			this.sounds.forEach((sound) => sound.stop());
 		}
 
-		if (Math.abs(beat % (TICK * 1536) - TICK * 892) < TICK / 2) {
+		if (Math.abs(beat % (TICK * 1792) - TICK * 892) < TICK / 2) {
 			this.setState({isShown: false});
 		}
 
-		if (Math.abs(beat % (TICK * 1536) - TICK * 1408) < TICK / 2) {
+		if (Math.abs(beat % (TICK * 1792) - TICK * 1408) < TICK / 2) {
+			this.setState({isShown: false});
+		}
+
+		if (Math.abs(beat % (TICK * 1792) - TICK * 1536) < TICK / 2) {
 			this.setState({isShown: false});
 		}
 
 		if (this.props.isPercussion) {
-			const isPlay = this.score.some((note) => Math.abs(note.time - beat % (TICK * 1536)) < TICK / 2 && note.type === 'note');
+			const isPlay = this.score.some((note) => Math.abs(note.time - beat % (TICK * 1792)) < TICK / 2 && note.type === 'note');
 
 			if (!isPlay) {
 				return;
@@ -83,10 +87,10 @@ module.exports = class Sound extends React.Component {
 
 			this.sounds[0].play();
 		} else {
-			const playNoteIndex = this.score.findIndex((note) => Math.abs(note.time - beat % (TICK * 1536)) < TICK / 2 && note.type === 'note');
-			const playNotes = this.score.filter((note) => Math.abs(note.time - beat % (TICK * 1536)) < TICK / 2 && note.type === 'note');
+			const playNoteIndex = this.score.findIndex((note) => Math.abs(note.time - beat % (TICK * 1792)) < TICK / 2 && note.type === 'note');
+			const playNotes = this.score.filter((note) => Math.abs(note.time - beat % (TICK * 1792)) < TICK / 2 && note.type === 'note');
 
-			if (playNotes.length !== 0 || (this.score[this.currentNote] && Math.abs(this.score[this.currentNote].time + this.score[this.currentNote].duration - beat % (TICK * 1536)) < TICK / 2)) {
+			if (playNotes.length !== 0 || (this.score[this.currentNote] && Math.abs(this.score[this.currentNote].time + this.score[this.currentNote].duration - beat % (TICK * 1792)) < TICK / 2)) {
 				this.sounds.forEach((sound) => sound.stop());
 			}
 
--- src/scores.js
+++ src/scores.js
@@ -8,6 +8,8 @@ const chorus1 = require('../score/chorus1.txt');
 const chorus2 = require('../score/chorus2.txt');
 const chorus3 = require('../score/chorus3.txt');
 const chorus4 = require('../score/chorus4.txt');
+const chorus5 = require('../score/chorus5.txt');
+const chorus6 = require('../score/chorus6.txt');
 const clap = require('../score/clap.txt');
 const closedHihat = require('../score/closed-hihat.txt');
 const cymbal = require('../score/cymbal.txt');
@@ -26,6 +28,8 @@ module.exports = {
 	chorus2,
 	chorus3,
 	chorus4,
+	chorus5,
+	chorus6,
 	clap,
 	'closed-hihat': closedHihat,
 	cymbal,
